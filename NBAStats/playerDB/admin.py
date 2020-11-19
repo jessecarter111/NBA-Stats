9a2fc424a958c9c40bc5bff59adaed2893a04eb0
Jesse <m.c.jesse@live.com>
2020-11-19 17:45:56 -0400

    Added the admin page

--- NBAStats/playerDB/admin.py
+++ NBAStats/playerDB/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Player, Team
+
 # Register your models here.
+
+class PlayerInline(admin.TabularInline):
+    model = Player
+
+class TeamAdmin(admin.ModelAdmin):
+    model = Team
+    inlines = [
+        PlayerInline,
+    ]
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Player)
